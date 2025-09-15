021c79de125e6805d8177f5c9d8fbdfbf0d491f5
Rafael Matos <rafaelmatos.dev1@gmail.com>
2025-09-15 07:50:08 -0300

    feat: add docs to the selection sort

--- src/sorting/selection_sort.c
+++ src/sorting/selection_sort.c
@@ -1,3 +1,20 @@
+/*
+Selection Sort is a comparison-based sorting algorithm. It sorts an array by repeatedly selecting the smallest (or largest) element from the unsorted portion and swapping it with the first unsorted element. This process continues until the entire array is sorted.
+
+Time complexity: O(n^2)
+
+=====================================================
+
+Advantages:
+    - Requires only a constant O(1) extra memory space
+    - It requires less number of swaps(or memory writes) compared to many other standard algorithms. Therefore it can be simple algorithm choice when memory writs are costly.
+
+Disadvantages:
+    - As Selection Sort has a time complexity of O(n^2) makes it slower compared to algorithms like Quick Sort or Merge Sort.
+    - Does not maintain the relative order of equal elements which means it is not stable.
+*/
+
+
 #include <stdio.h>
 
 void selection_sort(int arr[], int n) {
